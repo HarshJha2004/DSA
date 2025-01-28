be36cdbe5478430a6cf5f86cbb17344bce2f7ec1
HarshJha2004 <Harshwork5546@gmail.com>
2025-01-28 12:35:15 +0530

    function prcatices

--- Functions/main.cpp
+++ Functions/main.cpp
@@ -1,6 +1,25 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    cout << "Namast Duniya";
-}
+    void PrintNumbers(){
+        for(int i=1;i<=100;i++){
+            cout << i << endl;
+        }
+    }
+
+    int SimpleInterest(int P, int R, int T){
+        int ProcessInterest = (P*R*T)/100;
+        return ProcessInterest;
+    }
+
+    int PrintPrimeNumbers(int n){
+        // int Process = 
+
+    }
+
+    //Main function.
+    int main(){
+        int AnswerInterest = SimpleInterest(5000,5,5);
+        cout << AnswerInterest;
+    }
+    
